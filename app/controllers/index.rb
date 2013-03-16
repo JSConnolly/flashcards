e76186539a57e6499ee026f4c6f05b0f3ae70fb4
Apprentice <apprentice@devbootcamp.com>
2013-03-15 17:30:54 -0700

    controller first draft & views built out

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,53 +1,81 @@
+### INDEX
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
+### USERS
 
-get '/login'
-  @user = login(params[:login])
-  if @user  
-    redirect to '/user/' + @user.id
+get '/signup' do
+
+  erb :signup
+end
+
+post '/signup' do
+  @user = User.new(params[:signup]) 
+  unless @user.save
+    # errors
+  end
+  erb :profile
+end
+
+get '/login' do
+
+  erb :login
+end
+
+
+post '/login' do
+  authenticate(params[:email], params[:password])
+  @id = User.find_by_email(params[:email]).id
+  redirect '/profile/' + @id.to_s
+end
+
+get '/profile/:id' do
+  @user = User.find(params[:id])
+  if @token == @user.email
+    @user
+    erb :profile
   else
-    # throw error
+    redirect '/login'
   end
 end
 
 
 
-get '/user/:id' do
-  params[:id]
-  erb :profile
+get '/logout' do
+  session.clear
+  redirect '/'
 end
 
 
-get '/user/:id/:deck' do
-  
-  # play()
-  #   until the game is over, play
-  #   when done
-  #     erb :success
-  #   end
-  
+
+
+### GAME
+
+
+get '/game' do
+  params[:deck]
+  @round = Round.create
+  @round.deck << params[:deck]
+
+  @deck = params[:deck]
+  @card = @deck.cards.first
+
   erb :game
 
 end
 
+get '/user/id/deck/:card' do
 
 
-
-
-
-
-
-
-get '/signup' do
-  
-  erb :signup
-
+  erb :game
 end
 
-post 'user/new' do
-
-  erb :profile
+post '/guess' do
+  #logic about what to store/what card to show
 end
+
+
+
