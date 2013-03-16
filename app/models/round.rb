814027e984565901a80f6a31028b2427bebf1673
Apprentice <apprentice@devbootcamp.com>
2013-03-15 17:30:52 -0700

    add new seed file, some model methods

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,31 @@ class Round < ActiveRecord::Base
   belongs_to :decks
   has_many :guesses
   has_many :cards, through :decks
+
+  def initialize
+    @deck = Deck.find(deck_id)
+    @deck.shuffle!
+    @remaining_cards = @deck.cards
+  end
+
+  def next_card
+    @current_card = @remaining_cards.pop
+  end
+
+  def guess(input)
+    guess = Guess.create( guess: input, round_id: id, card_id: @current_card.id)
+    guess.check ? win_card : lose_card
+  end
+
+  def win_card
+    num_correct += 1
+  end
+
+  def lose_card
+    num_incorrect += 1
+  end
+
+
+
+
 end
