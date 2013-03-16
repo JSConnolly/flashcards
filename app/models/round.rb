8a3cc0f6dc5661813d31e2ab333b829ec3e637af
Apprentice <apprentice@devbootcamp.com>
2013-03-16 16:36:49 -0700

    working core features

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,6 @@
 class Round < ActiveRecord::Base
-  attr_reader :current_card, :last_try
+
+  attr_reader :current_card, :last_try, :first_card, :deck, :deck_length
   attr_accessor :remaining_cards
   belongs_to :users
   belongs_to :decks
@@ -11,16 +12,32 @@ class Round < ActiveRecord::Base
   end
 
   def prepare(n=-1)
-    deck_cards = Deck.find(deck_id).cards
+    n = n.to_i
+    starting_deck = deck_cards
     if n == -1
-      @remaining_cards = deck_cards
+      @remaining_cards = starting_deck
       @last_try = "true"
     else
-      to_pop = (deck_cards.length - n.to_i)
-      @remaining_cards= deck_cards
-      @remaining_cards.pop(to_pop)
+      cards_to_pop = (starting_deck.length - n)
+      @remaining_cards = starting_deck
+      @current_card = starting_deck[n]
+      @remaining_cards.pop(cards_to_pop)
+      puts '*' * 400
+      puts deck_length
+      puts @remaining_cards.length
+      @first_card = (@remaining_cards.length == deck_length)
+      # puts '*' *200
+      # puts @current_card.inspect
     end
 
+  end
+
+  def deck_length
+    @deck_length = Deck.find(deck_id).cards.length
+  end
+
+  def deck_cards
+    @deck ||= Deck.find(deck_id).cards
   end
   
   def done?
@@ -42,11 +59,13 @@ class Round < ActiveRecord::Base
 
   def win_card
     self.num_correct += 1
+    self.save
     @last_try = true
   end
 
   def lose_card
     self.num_incorrect += 1
+    self.save
     @last_try = false
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,15 @@ require 'CSV'
 #   Card.create(question: row[0], answer: row[1], deck_id: capitals.id)
 # end
 
-jQuery_e_methods = Deck.create(name: "jQuery Event Methods")
+# jQuery_e_methods = Deck.create(name: "jQuery Event Methods")
 
-method_defs_parsed = CSV.read("public/jQuery.csv")
-method_defs_parsed.each do |row|
-  Card.create(question: row[1], answer: row[0], deck_id: jQuery_e_methods.id)
-end
+# method_defs_parsed = CSV.read("public/jQuery.csv")
+# method_defs_parsed.each do |row|
+#   Card.create(question: row[1], answer: row[0], deck_id: jQuery_e_methods.id)
+# end
+
+test_deck = Deck.create( name: "Test Deck" )
+
+Card.create( question: 'test q1', answer: 'answer a1', deck_id: test_deck.id)
+Card.create( question: 'test q2', answer: 'answer a2', deck_id: test_deck.id)
+Card.create( question: 'test q3', answer: 'answer a3', deck_id: test_deck.id)
